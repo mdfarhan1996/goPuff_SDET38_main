46e17fedc8d90f97f5ec6e1cfd54886d87757126
FARHAN <FARHAN@DESKTOP-1DLRFCA>
2022-08-27 13:28:15 +0530

    newProgramforpractise

--- src/main/java/com/goPuff/testng/NewProgramForgit22.java
+++ src/main/java/com/goPuff/testng/NewProgramForgit22.java
@@ -1,8 +1,10 @@
 package com.goPuff.testng;
 
-public class NewProgramForgit {
+public class NewProgramForgit22 {
 
 	public static void main(String[] args) {
+System.out.println("farhannajkhan");
+System.out.println("farhannajkhan");
 System.out.println("farhannajkhan");
 	}
 
